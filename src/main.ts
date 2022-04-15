c4ab6e145ebcb1966ce8e515dfc9f24c57eb815b
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-04-15 10:41:19 +0200

    feat: adds PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue);
 
-app.mount('#app')
+app.mount('#app');
