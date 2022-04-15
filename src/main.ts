2dfe755ad4ac14869e1f2dd015fe751f94aeaa88
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-04-15 16:27:18 +0200

    feat: adds primevue page

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 
+import "primeflex/primeflex.css";
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/tailwind-light/theme.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
