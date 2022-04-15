46d7e75b995789f8e83fea81ff0a5cbef5cb1daf
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-04-15 12:50:49 +0200

    revert: reverts airbnb from eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
-    "airbnb-base",
-    "airbnb-typescript/base"
+    "@vue/eslint-config-prettier"
   ],
   "env": {
     "vue/setup-compiler-macros": true
