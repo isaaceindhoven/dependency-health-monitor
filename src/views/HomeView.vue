bc071f9e6d1234728d6da7152901b7c4a60ad025
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-04-21 19:19:49 +0200

    refactor: simplified home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,7 @@
 <template>
-  <div class="grid justify-content-center">
-    <!-- <SelectButton v-model="selectedOption" :options="options" optionLabel="text" optionValue="name" /> -->
-    <div class="col-4" v-for="option in options" :key="option.name">
-      <SelectButtonCard :name="option.name" :text="option.text" v-model="selectedOption" />
-    </div>
-  </div>
+  <div>This is a cool home page maybe</div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-import SelectButton from 'primevue/selectbutton';
-import SelectButtonCard from '../components/SelectButtonCard.vue';
-
-let selectedOption = ref('GitHub');
-const options = [
-  {
-    name: 'GitHub',
-    text: 'Log in and select a repository',
-  },
-  {
-    name: 'Upload',
-    text: 'Upload or paste a package.json',
-  },
-  {
-    name: 'Public URL',
-    text: 'Run report using public repository URL',
-  },
-];
-
-const printNewValue = (newValue) => {
-  selectedOption.value = newValue;
-};
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
