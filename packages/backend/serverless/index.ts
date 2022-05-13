4bf6828f13527565fd81138fa70bba2600dc99f7
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-05-13 21:03:26 +0200

    feat: uses dependency fetcher

--- packages/backend/serverless/index.ts
+++ packages/backend/serverless/index.ts
@@ -1,15 +1,22 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import fetchDependenciesForPackageInDepth from '@dependency-health-monitor/npm-dependency-fetcher';
+import type { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log('HTTP trigger function processed a request.');
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
+  if (!req.body || !req.body.packageName) {
+    context.res = {
+      status: 401,
+      body: 'Please specify a packageName key in your body.',
+    };
+    return;
+  }
+  const packageName: string = req.body.packageName || '';
+
+  const dependencies: Map<string, number> = await fetchDependenciesForPackageInDepth(packageName);
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: responseMessage,
+    body: Object.fromEntries(dependencies),
+    // body: packageName,
   };
 };
 
