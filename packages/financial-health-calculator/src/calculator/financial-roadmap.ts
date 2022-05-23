d4e7ebe53e5ddac76a2ecf3f6eec464b345b2149
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-05-23 19:29:33 +0200

    feat: calculates score for financial roadmap

---
+++ packages/financial-health-calculator/src/calculator/financial-roadmap.ts
@@ -0,0 +1,21 @@
+import { ScoreCalculationResult } from './../types/score-calculation-result';
+
+export const calculateFinancialRoadmapScore = (
+  packageName: string,
+  fundingGoalCents: number,
+  currency: string,
+): ScoreCalculationResult => {
+  if (!fundingGoalCents) {
+    return {
+      score: 0,
+      explanation: `${packageName} has not set any funding goals. Therefore, they receive a score of 0.`,
+    };
+  }
+
+  return {
+    score: 100,
+    explanation: `${packageName} has set a Funding goal of ${currency} ${
+      fundingGoalCents / 100
+    }. Therefore, they receive a score of 100.`,
+  };
+};
