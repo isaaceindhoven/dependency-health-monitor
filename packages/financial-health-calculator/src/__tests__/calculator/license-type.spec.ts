0961f539c02ca6a6e240fba0129f5e28c435cbb6
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-05-26 13:00:20 +0200

    test: adds license type calculation test

---
+++ packages/financial-health-calculator/src/__tests__/calculator/license-type.spec.ts
@@ -0,0 +1,28 @@
+import { calculateLicenseTypeScore } from './../../calculator/license-type';
+import { describe, it, expect } from 'vitest';
+
+describe('license type score calculation', () => {
+  it('gives a score of 0 whenever the license is in the SPDX list', async () => {
+    expect(() => {
+      const scoreCalculationResult = calculateLicenseTypeScore('eslint', 'MIT');
+
+      expect(scoreCalculationResult.score).toBe(0);
+    }).not.toThrow();
+  });
+
+  it('gives a score of 100 whenever the license is not the SPDX list', async () => {
+    expect(() => {
+      const scoreCalculationResult = calculateLicenseTypeScore('eslint', 'MITLPDEA');
+
+      expect(scoreCalculationResult.score).toBe(100);
+    }).not.toThrow();
+  });
+
+  it('gives a score of 0 whenever no license has been set in NPM', async () => {
+    expect(() => {
+      const scoreCalculationResult = calculateLicenseTypeScore('eslint', 'none');
+
+      expect(scoreCalculationResult.score).toBe(0);
+    }).not.toThrow();
+  });
+});
