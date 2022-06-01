e1b8121ba189f53e13fbdecbb3e712327a980422
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-06-01 21:03:34 +0200

    feat: uses spdx license list package

--- packages/financial-health-calculator/src/calculator/license-type.ts
+++ packages/financial-health-calculator/src/calculator/license-type.ts
@@ -1,5 +1,5 @@
+import spdxLicenseList from 'spdx-license-list';
 import { CRITERIA_WEIGHTS } from './../constants.js';
-import { spdxLicenseList } from '../data-files/spdx-licenses.js';
 import { cannotCalculateWithMissingData } from './../helpers/missing-data-for-calculation.js';
 import type { ScoreCalculationResult } from './../types/score/score-calculation-result';
 
@@ -8,15 +8,12 @@ export const calculateLicenseTypeScore = (packageName: string, licenseIdentifier
     return cannotCalculateWithMissingData(packageName, 'License type', ['License type']);
   }
 
-  const licenses = spdxLicenseList.licenses;
-  for (const license of licenses) {
-    if (license.licenseId === licenseIdentifier) {
-      return {
-        score: 0,
-        weightedScore: 0,
-        explanation: `License with ID ${licenseIdentifier} has been found in the SPDX list. Therefore, the license is seen as free to use and ${packageName} receives a score of 0 for the 'License type' criterion.`,
-      };
-    }
+  if (spdxLicenseList[licenseIdentifier]) {
+    return {
+      score: 0,
+      weightedScore: 0,
+      explanation: `License with ID ${licenseIdentifier} has been found in the SPDX list. Therefore, the license is seen as free to use and ${packageName} receives a score of 0 for the 'License type' criterion.`,
+    };
   }
 
   return {
