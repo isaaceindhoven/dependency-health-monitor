bc848877def0d0620a0e947782cfd69161a97baf
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-06-02 13:48:12 +0200

    test: tests financial roadmap score

---
+++ packages/financial-health-calculator/src/__tests__/calculator/financial-roadmap.spec.ts
@@ -0,0 +1,27 @@
+import { calculateFinancialRoadmapScore } from './../../calculator/financial-roadmap';
+import { CRITERIA_WEIGHTS } from './../../constants';
+import { describe, it, expect } from 'vitest';
+
+describe('financial roadmap score calculation', () => {
+  it('gives a score of 100 whenever the project has defined a funding goal on their OpenCollective page', async () => {
+    expect(() => {
+      const fundingGoal = 500_000_000;
+      const currency = 'USD';
+      const scoreCalculationResult = calculateFinancialRoadmapScore('eslint', fundingGoal, currency);
+
+      expect(scoreCalculationResult.score).toBe(100);
+      expect(scoreCalculationResult.weightedScore).toBe(100 * CRITERIA_WEIGHTS.FINANCIAL_ROADMAP);
+    }).not.toThrow();
+  });
+
+  it('gives a score of 0 whenever the project has not defined a funding goal on their OpenCollective page', async () => {
+    expect(() => {
+      const fundingGoal = 0;
+      const currency = '';
+      const scoreCalculationResult = calculateFinancialRoadmapScore('eslint', fundingGoal, currency);
+
+      expect(scoreCalculationResult.score).toBe(0);
+      expect(scoreCalculationResult.weightedScore).toBe(0 * CRITERIA_WEIGHTS.FINANCIAL_ROADMAP);
+    }).not.toThrow();
+  });
+});
