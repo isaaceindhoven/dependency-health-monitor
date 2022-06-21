2fc36599844cb4c85b964ae035dfea5f4e981c9a
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-06-21 18:31:03 +0200

    feat: adds home page

--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>This is a cool home page maybe</div>
+  <div class="grid pt-6">
+    <div class="col-6">
+      <h1 class="text-6xl line-height-2 font-semibold mb-2">Help improve Open-Source sustainability!</h1>
+      <h4 class="text-2xl font-light mb-4">
+        A lot of Open-Source projects suffer from financial sustainability and face the risk of not getting maintained
+        due to a lack of resources. Use the Dependency Health Monitor to view the financial state of your dependencies
+        and support your dependency!
+      </h4>
+      <Button @click="redirectToExecuteReportPage" label="Execute the report!" icon="pi pi-heart" />
+    </div>
+    <div id="help-improve" class="col-6">
+      <img v-if="darkMode" src="@/assets/help-improve-dark.svg" />
+      <img v-else src="@/assets/help-improve-light.svg" />
+      <div class="flex justify-content-center">
+        <Button
+          @click="openStorySetPage"
+          label="People illustrations by Storyset"
+          class="p-button-text"
+          icon="pi pi-external-link"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import router from '@/router';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+
+const darkMode = ref(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+const redirectToExecuteReportPage = () => {
+  router.push({ path: '/run-report/select' });
+};
+
+const openStorySetPage = () => {
+  window.open('https://storyset.com/people');
+};
+</script>
 
 <style lang="scss" scoped></style>
