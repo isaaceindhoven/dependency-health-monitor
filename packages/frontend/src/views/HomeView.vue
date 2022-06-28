7c4733217c8f0f384c72d18f886503816ff1a733
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-06-28 14:38:09 +0200

    refactor: uses HTML a tag only

--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -7,15 +7,22 @@
         due to a lack of resources. Use the Dependency Health Monitor to view the financial state of your dependencies
         and support your dependency!
       </h4>
-      <a class="no-underline" href="/run-report/select">
-        <Button label="Execute the report!" icon="pi pi-heart" />
+      <a class="p-button p-component no-underline" href="/run-report/select">
+        <span class="pi pi-heart p-button-icon p-button-icon-left"></span
+        ><span class="p-button-label">Execute the report!</span>
       </a>
     </div>
     <div id="help-improve" class="col-6">
       <IconHelpImproveVue />
       <div class="flex justify-content-center">
-        <a class="no-underline" href="https://storyset.com/people" target="_blank" rel="noopener noreferer">
-          <Button label="People illustrations by Storyset" class="p-button-text" icon="pi pi-external-link" />
+        <a
+          class="p-button p-component p-button-text no-underline"
+          href="https://storyset.com/people"
+          target="_blank"
+          rel="noopener noreferer"
+        >
+          <span class="pi pi-external-link p-button-icon p-button-icon-left mr-3"></span
+          ><span class="p-button-label">People illustrations by Storyset</span>
         </a>
       </div>
     </div>
