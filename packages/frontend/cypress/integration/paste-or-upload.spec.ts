a1a9783e02aac628d356fa058ef0583ef37a9c3c
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-06-28 18:42:33 +0200

    test: updates test with new UI

--- packages/frontend/cypress/integration/paste-or-upload.spec.ts
+++ packages/frontend/cypress/integration/paste-or-upload.spec.ts
@@ -45,16 +45,8 @@ describe('Paste or upload page', () => {
     cy.get('.grid-nogutter > :nth-child(1)').contains('Submit & Execute report');
     cy.get('.grid-nogutter > :nth-child(1)').should('be.disabled');
 
-    // Confirm that the upload button is disabled
-    cy.get('.p-fileupload-buttonbar > :nth-child(2)').should('contain.text', 'Upload');
-    cy.get('.p-fileupload-buttonbar > :nth-child(2)').should('be.disabled');
-
     // Upload file to the input field
     cy.get('input[type="file"]').attachFile('../fixtures/example.package.json');
-
-    // Check if upload button is enabled & click it
-    cy.get('.p-fileupload-buttonbar > :nth-child(2)').should('not.be.disabled');
-    cy.get('.p-fileupload-buttonbar > :nth-child(2)').click();
 
     // Confirm that next button is enabled
     cy.get('.grid-nogutter > :nth-child(1)').contains('Submit & Execute report');
