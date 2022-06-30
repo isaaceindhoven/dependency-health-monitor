c5e8dd9aa5cc8867c31d839b1b52aafdd3cc5c4f
Muhammed Ensar Ishakoglu <ishakoglu.ensar@gmail.com>
2022-06-30 15:22:29 +0200

    refactor: removes comments

--- packages/frontend/src/views/about/AboutView.vue
+++ packages/frontend/src/views/about/AboutView.vue
@@ -1,13 +1,4 @@
 <template>
-  <!-- <div>
-    <h1>Dependency Health Monitor</h1>
-    <h4>
-      The Dependency Health Monitor is an application that is capable of determining the financial health of your
-      dependencies in an equitable manner. To determine the financial health, several criteria have been set up. We
-      believe these criteria are capable of describing the financial health of a project. To find out what criteria are
-      used and how the equity of the financial health is determined, check the links below!
-    </h4>
-  </div> -->
   <router-view></router-view>
 </template>
 
